9efca7508365bdb1097db3b158bbaa33f32ca9b9
Matthew M. Emma <ibuilder@users.noreply.github.com>
2025-04-29 21:27:15 -0400

    header footer

---
+++ templates/footer.php
@@ -0,0 +1,13 @@
+        </main>
+        <footer class="text-center mt-5 text-muted">
+            <p>&copy; <?php echo date('Y'); ?> Your Company Name</p>
+        </footer>
+    </div> <!-- Close Bootstrap container -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
